6c9024ca96477054bbb9fa3b955c2420a716b21d
Fandy Kuncoro <fandy19061999@gmail.com>
2019-01-09 08:34:01 +0700

    auto redirect ke home jika not found

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,22 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        if($this->isHttpException($exception)){
+            switch($exception->getStatusCode())
+            {
+                case '404':
+                    return redirect()->guest('pages.home');
+                    break;
+                case '500':
+                    return redirect()->guest('pages.home');
+                    break;
+                default:
+                    return $this->renderHttpException($exception);
+                    break;
+            }
+        }
+        else{
+            return parent::render($request, $exception);
+        }
     }
 }
