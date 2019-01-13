5a8fadd2ee86bd2b1b862b58f195fcd41ce30014
Fandy Kuncoro <fandy19061999@gmail.com>
2019-01-14 00:43:48 +0700

    edit navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,68 +1,19 @@
-<nav class="navbar navbar-expand-lg navbar-light navbar-laravel">
-  <div class="container">
-      <a class="navbar-brand" href="{{ url('/') }}">
-        {{ config('app.name', 'IRIS ITS') }}
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+<header id="header">
+    <div class="container-fluid">
+        <div id="logo" class="pull-left">
+            <h1><a href="/"><img src="img/logo.png" alt="Logo" style="width: 135px"/></a></h1>
+        </div>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <!-- Left Side Of Navbar -->
-        <ul class="navbar-nav mr-auto">
+        <nav id="nav-menu-container">
+            <ul class="nav-menu">
+                <li><a href="/">Home</a></li>
+                <li><a href="/team">Team</a></li>
+                <li><a href="/achievement">Achievement</a></li>
+                <li><a href="/gallery">Gallery</a></li>
+                <li><a href="/qualification">Qualification</a></li>
+                <li><a href="/about">About</a></li>
+            </ul>
+        </nav>
 
-        </ul>
-
-        <!-- Right Side Of Navbar -->
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="{{'/'}}">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{'/team'}}">Team</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{'/achievement'}}">Achievement</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{'/gallery'}}">Gallery</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{'/qualification'}}">Qualification</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{'/about'}}">About</a>
-            </li>
-          <!-- Authentication Links -->
-            {{-- @guest
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                </li>
-                @if (Route::has('register'))
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                    </li>
-                @endif
-            @else
-                <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ Auth::user()->name }} <span class="caret"></span>
-                    </a>
-
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                           onclick="event.preventDefault();
-                                         document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </div>
-                </li>
-            @endguest --}}
-        </ul>
     </div>
-  </div>
-</nav>
+</header><!-- #header -->
