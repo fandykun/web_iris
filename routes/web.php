4566de780c60bfade933daea176a9ef9f622c75b
Fandy Kuncoro <fandy19061999@gmail.com>
2019-01-13 13:28:56 +0700

    edit route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PagesController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,7 +12,14 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Auth::routes();
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@index');
+Route::get('/team', 'PagesController@team');
+Route::get('/achievement', 'PagesController@achievement');
+Route::get('/gallery', 'PagesController@gallery');
+Route::get('/qualification', 'PagesController@qualification');
+Route::get('/about', 'PagesController@about');
+
+// Route::resource('gallery', 'GalleryController');
+// Route::resource('team', 'TeamController');
