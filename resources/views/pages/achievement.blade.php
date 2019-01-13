58d49e3e76fc365ad930107eaa4694204a665fa9
Fandy Kuncoro <fandy19061999@gmail.com>
2019-01-14 00:43:57 +0700

    add pages

---
+++ resources/views/pages/achievement.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title')
+    <title>{{$title}} - {{ config('app.name') }}</title>
+@endsection
+
+@section('content')
+<div class="container">
+    
+</div>
+@endsection
