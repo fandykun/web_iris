ee487ffe9e77b95628dc3b7da6f948ad76af75a2
Fandy Kuncoro <fandy19061999@gmail.com>
2019-01-14 20:39:37 +0700

    edit home + footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,60 +1,42 @@
 <footer id="footer">
-        <div class="footer-top">
-          <div class="container">
-            <div class="row">
-    
-              <div class="col-lg-3 col-md-6 footer-info">
-                <h3>BizPage</h3>
-                <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus. Scelerisque felis imperdiet proin fermentum leo. Amet volutpat consequat mauris nunc congue.</p>
-              </div>
-    
-              <div class="col-lg-3 col-md-6 footer-links">
-                <h4>Useful Links</h4>
-                <ul>
-                  <li><i class="ion-ios-arrow-right"></i> <a href="#">Home</a></li>
-                  <li><i class="ion-ios-arrow-right"></i> <a href="#">About us</a></li>
-                  <li><i class="ion-ios-arrow-right"></i> <a href="#">Services</a></li>
-                  <li><i class="ion-ios-arrow-right"></i> <a href="#">Terms of service</a></li>
-                  <li><i class="ion-ios-arrow-right"></i> <a href="#">Privacy policy</a></li>
-                </ul>
-              </div>
-    
-              <div class="col-lg-3 col-md-6 footer-contact">
-                <h4>Contact Us</h4>
-                <p>
-                  A108 Adam Street <br>
-                  New York, NY 535022<br>
-                  United States <br>
-                  <strong>Phone:</strong> +1 5589 55488 55<br>
-                  <strong>Email:</strong> info@example.com<br>
-                </p>
-    
-                <div class="social-links">
-                  <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
-                  <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
-                  <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
-                  <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
-                  <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
-                </div>
-    
-              </div>
-    
-              <div class="col-lg-3 col-md-6 footer-newsletter">
-                <h4>Our Newsletter</h4>
-                <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna veniam enim veniam illum dolore legam minim quorum culpa amet magna export quem marada parida nodela caramase seza.</p>
-                <form action="" method="post">
-                  <input type="email" name="email"><input type="submit"  value="Subscribe">
-                </form>
-              </div>
-    
-            </div>
+  <div class="footer-top">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 footer-info">
+          <h3>IRIS-ITS</h3>
+          <p>IRIS is one of the Sepuluh Nopember Institute of Technology Robotic Team build in 2016.
+            We work on Intelligent Wheeled Robot Development Research, which has ability to play football automatically.</p>
+        </div>
+        <div class="col-lg-6 col-md-6 footer-links">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1978.838995163011!2d112.79762184786317!3d-7.277432886364895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fa11d96fdfe5%3A0x329c27ff3fca8170!2sUKM+Robotika+ITS!5e0!3m2!1sen!2sid!4v1484736564728" width="100%" height="280" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+
+        <div class="col-lg-3 col-md-6 footer-contact">
+          <h4>Contact Us</h4>
+          <p>
+            UKM Robotika ITS<br>
+            Sukolilo, Surabaya City 60117<br>
+            East Java, Indonesia <br>
+            <strong>Phone:</strong> +6281259606366<br>
+            <strong>Email:</strong> iris.krsbi@gmail.com<br>
+          </p>
+
+          <div class="social-links">
+            <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+            <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+            <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+            <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
+            <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
           </div>
         </div>
-    
-        <div class="container">
-          <div class="copyright">
-            &copy; Copyright <strong>IRIS ITS</strong>. All Rights Reserved
-          </div>
-          <div class="credits">2019</div>
-        </div>
-      </footer>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="copyright">
+      &copy; Copyright <strong>IRIS ITS</strong>. All Rights Reserved
+    </div>
+    <div class="credits">2019</div>
+  </div>
+</footer>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -10,7 +10,6 @@
 <section id="intro">
     <div class="intro-container">
         <div id="introCarousel" class="carousel  slide carousel-fade" data-ride="carousel">
-        {{-- <div class="carousel-inner" role="listbox"> --}}
             <div class="carousel-item active">
                 <div class="carousel-background"><img src="img/background3.jpg" alt=""></div>
                 <div class="carousel-container">
@@ -21,7 +20,7 @@
                         <a href="#about" class="btn-get-started scrollto">Get Started</a>
                     </div>
                 </div>
-            {{-- </div> --}}
+            </div>
         </div>
     </div>
 </section>
@@ -29,59 +28,46 @@
 <!--==========================
     About Us Section
 ============================-->
-<section id="about">
+<section id="services">
     <div class="container">
-
         <header class="section-header">
             <h3>About Us</h3>
+            <p>IRIS is one of the Sepuluh Nopember Institute of Technology Robotic Team build in 2016.<br>
+                We work on Intelligent Wheeled Robot Development Research, which has ability to play football automatically.</p>
         </header>
 
-        <div class="row about-cols">
+        <div class="row">
 
-            <div class="col-md-4 wow fadeInUp">
-                <div class="about-col">
-                    <div class="img">
-                    <img src="img/background4.jpg" alt="" class="img-fluid">
-                    <div class="icon"><i class="ion-ios-speedometer-outline"></i></div>
-                    </div>
-                    <h2 class="title"><a href="#">Our Mission</a></h2>
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </p>
-                </div>
+            <div class="col-lg-4 col-md-6 box wow bounceInUp" data-wow-duration="1.2s">
+              <div class="icon"><i class="ion-ios-person"></i></div>
+              <h4 class="title"><a>Coach</a></h4>
+              <p class="description">The IRIS-ITS Coach himself would be in charge of leading the assembly process of the robot that will participate in competition.</p>
             </div>
-
-            <div class="col-md-4 wow fadeInUp" data-wow-delay="0.1s">
-                <div class="about-col">
-                    <div class="img">
-                    <img src="img/background4.jpg" alt="" class="img-fluid">
-                    <div class="icon"><i class="ion-ios-list-outline"></i></div>
-                    </div>
-                    <h2 class="title"><a href="#">Our Plan</a></h2>
-                    <p>
-                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem  doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-                    </p>
-                </div>
+            <div class="col-lg-4 col-md-6 box wow bounceInUp" data-wow-duration="1.2s">
+              <div class="icon"><i class="ion-code"></i></div>
+              <h4 class="title"><a>Programming Division</a></h4>
+              <p class="description">This division is in charge of creating a program that run on the robot according to the wanted command.</p>
             </div>
-
-            <div class="col-md-4 wow fadeInUp" data-wow-delay="0.2s">
-                <div class="about-col">
-                    <div class="img">
-                    <img src="img/background4.jpg" alt="" class="img-fluid">
-                    <div class="icon"><i class="ion-ios-eye-outline"></i></div>
-                    </div>
-                    <h2 class="title"><a href="#">Our Vision</a></h2>
-                    <p>
-                    Nemo enim ipsam voluptatem quia voluptas sit aut odit aut fugit, sed quia magni dolores eos qui ratione voluptatem sequi nesciunt Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.
-                    </p>
-                </div>
+            <div class="col-lg-4 col-md-6 box wow bounceInUp" data-wow-duration="1.2s">
+              <div class="icon"><i class="ion-ios-bolt"></i></div>
+              <h4 class="title"><a>Electronic Division</a></h4>
+              <p class="description">This division performs its duties on the electronic part of the robot. They assemble electronic components that will support the robot working system.</p>
             </div>
-
-        </div>
-
+            <br><br>
+            <div class="col-lg-2 col-md-6">
+            </div>
+            <div class="col-lg-4 col-md-6 box wow bounceInUp" data-wow-delay="0.1s" data-wow-duration="1.2s">
+              <div class="icon"><i class="ion-settings"></i></div>
+              <h4 class="title"><a>Mechanics Division</a></h4>
+              <p class="description">This division is in charge of designing the robot, and also make sure the design built as good as expected.</p>
+            </div>
+            <div class="col-lg-4 col-md-6 box wow bounceInUp" data-wow-delay="0.1s" data-wow-duration="1.2s">
+              <div class="icon"><i class="ion-clipboard"></i></div>
+              <h4 class="title"><a>Official Division</a></h4>
+              <p class="description">The division regulates all the records, correspondence, sponsorship, and so forth.</p>
+            </div>
+          </div>
     </div>
 </section>
-    
-
 
 @endsection
