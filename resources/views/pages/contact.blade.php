2a39897aa33de5bbe24c688a2f689891f3dba8b1
fandykun <fandy19061999@gmail.com>
2019-08-23 09:24:28 +0700

    adding templates

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -40,6 +40,112 @@
 </div>
 </section>
 <!--================Hero Banner Section end =================-->
+
+  <!-- ================ contact section start ================= -->
+  <section class="section-margin">
+    <div class="container">
+      <div class="d-none d-sm-block mb-5 pb-4">
+        <div id="map" style="height: 480px;"></div>
+        <script>
+          function initMap() {
+            var uluru = {lat: -7.277379, lng: 112.798304};
+            var map = new google.maps.Map(document.getElementById('map'), {
+              zoom: 17,
+              center: uluru,
+            });
+            var contentString = '<div>'+
+                '<div>'+
+                '</div>'+
+                '<h5>Sekretariat Robotika ITS</h5>'+
+                '<div style="font-size:16px">'+
+                '<p>Jl. Teknik Kimia, Kampus ITS<br>' +
+                'Sukolilo, Surabaya</p>' +
+                '</div>'+
+                '</div>';
+
+            var infowindow = new google.maps.InfoWindow({
+              content: contentString,
+            });
+
+            var marker = new google.maps.Marker({
+              position: uluru,
+              map: map,
+              title: 'Robotika ITS'
+            });
+            infowindow.open(map, marker);
+            marker.addListener('click', function() {
+              infowindow.open(map, marker);
+            });
+          }   
+          
+        </script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAc6pcTINhSJDXqrJ_09vrYkTZTKX0_y5c&callback=initMap"></script>
+        
+      </div>
+
+
+      <div class="row">
+        <div class="col-12">
+          <h2 class="contact-title">Get in Touch</h2>
+        </div>
+        <div class="col-lg-8">
+          <form class="form-contact contact_form" action="contact_process.php" method="post" id="contactForm" novalidate="novalidate">
+            <div class="row">
+              <div class="col-12">
+                <div class="form-group">
+                    <textarea class="form-control w-100" name="message" id="message" cols="30" rows="9" placeholder="Enter Message"></textarea>
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="form-group">
+                  <input class="form-control" name="name" id="name" type="text" placeholder="Enter your name">
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="form-group">
+                  <input class="form-control" name="email" id="email" type="email" placeholder="Enter email address">
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="form-group">
+                  <input class="form-control" name="subject" id="subject" type="text" placeholder="Enter Subject">
+                </div>
+              </div>
+            </div>
+            <div class="form-group mt-3">
+              <button type="submit" class="button button-contactForm">Send Message</button>
+            </div>
+          </form>
+        </div>
+
+        <div class="col-lg-4">
+          <div class="media contact-info">
+            <span class="contact-info__icon"><i class="ti-home"></i></span>
+            <div class="media-body">
+              <h3>Buttonwood, California.</h3>
+              <p>Rosemead, CA 91770</p>
+            </div>
+          </div>
+          <div class="media contact-info">
+            <span class="contact-info__icon"><i class="ti-tablet"></i></span>
+            <div class="media-body">
+              <h3><a href="tel:454545654">00 (440) 9865 562</a></h3>
+              <p>Mon to Fri 9am to 6pm</p>
+            </div>
+          </div>
+          <div class="media contact-info">
+            <span class="contact-info__icon"><i class="ti-email"></i></span>
+            <div class="media-body">
+              <h3><a href="mailto:support@colorlib.com">support@colorlib.com</a></h3>
+              <p>Send us your query anytime!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+	<!-- ================ contact section end ================= -->
+
 @endsection
 
 @section('script')
